469ad33a4851d9ce114b19a69770477f4e60432d
Sanavixx <echampa33088@gmail.com>
2020-08-25 08:26:00 +0430

    Update Get-MappedPrinters.ps1

--- Get-MappedPrinters.ps1
+++ Get-MappedPrinters.ps1
@@ -1,46 +1,56 @@
+Function Get-MappedPrinters {
+
 <#
 .SYNOPSIS
-Checks for printers current user is mapped to on the network (must be logged on).
+Checks for printers current user is mapped to on the network.
+
+.DESCRIPTION
+Checks for printers current user is mapped to on the network. User must be logged on.
 
 .PARAMETER ComputerName
-Specifies the computer user is on.
-
-.INPUTS
-None. You cannot pipe objects to Get-MappedPrinters.ps1.
-
-.OUTPUTS
-Registry key property.
+Mandatory. Specifies the computer user is logged on to.
 
 .EXAMPLE
 .\Get-MappedPrinters -ComputerName TESTPCNAME
 
 .NOTES
 Author: Edward Champa
-Date Modified: 24 August 2020
+Date Modified: 25 August 2020
 #>
 
-[Cmdletbinding()]
-Param(
-
-    [Parameter(Mandatory)]
-    [string]
-    $ComputerName
-)
-
-$ID = Get-WmiObject -Class win32_computersystem -ComputerName $ComputerName |
-Select-Object -ExpandProperty Username |
-ForEach-Object { ([System.Security.Principal.NTAccount]$_).Translate([System.Security.Principal.SecurityIdentifier]).Value }
-
-$Path = "Registry::\HKEY_USERS\$ID\Printers\Connections\"
-
-$Result = Invoke-Command -Computername $ComputerName -ScriptBlock {
-    param($Path)
+    [Cmdletbinding()]
     
-    Get-Childitem $Path | Select PSChildName
-} -ArgumentList $Path | Select -Property *
+    Param(
+    
+        [Parameter(Mandatory)]
+        [string]
+        $ComputerName
+    )
+    
+    Begin {
+    }
 
-Write-Host "`nPrint Server/Printer:"
+    Process {
 
-$Result = $Result.PSChildName -Split(",")
+        $ID = Get-WmiObject -Class win32_computersystem -ComputerName $ComputerName |
+        Select-Object -ExpandProperty Username |
+        ForEach-Object { ([System.Security.Principal.NTAccount]$_).Translate([System.Security.Principal.SecurityIdentifier]).Value }
+        
+        $Path = "Registry::\HKEY_USERS\$ID\Printers\Connections\"
+        
+        $Result = Invoke-Command -Computername $ComputerName -ScriptBlock {
+            param($Path)
+            
+            Get-Childitem $Path | Select PSChildName
+        } -ArgumentList $Path | Select -Property *
+        
+        Write-Host "`nPrint Server/Printer:"
+        
+        $Result = $Result.PSChildName -Split(",")
+        
+        $Result.Trim()
+    }
 
-$Result.Trim()
+    End {
+    }
+}
